a02b36c842d3ae7459bb91dae2a7f8f9705a083d
Abdullah-spec-art <m.abdullah9921@gmail.com>
2024-11-19 08:21:48 +0000

    load_jobs_from_db

--- app.py
+++ app.py
@@ -1,34 +1,38 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from database import JOBS
+from create_db import my_cursor,mydb, load_jobs_from_db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:System@12345/@localhost/job_details'
 
-JOBS = [{
-    'id': 1,
-    'title': "Data Analysis",
-    'Location': 'Lahore, Pakistan',
-    'salary': " Rs.10,00,000"
-}, {
-    'id': 2,
-    'title': "Data Science",
-    'Location': 'Islamabad, Pakistan',
-    'salary': " Rs.15,00,000"
-}, {
-    'id': 3,
-    'title': "Frontend",
-    'Location': 'Gujranwala, Pakistan'
-}, {
-    'id': 4,
-    'title': "Backend",
-    'Location': 'Lahore, Pakistan',
-    'salary': " Rs.12,00,000"
-}]
 
 
 @app.route("/")
 def hello_world():
+    jobs=load_jobs_from_db()
     return render_template("home.html",
-                           jobs=JOBS)
+                           jobs=jobs)
 
+
+'''@app.route("/job/<int:job_id>")
+def job_details(job_id):
+    # Find the job by ID
+    job = next((job for job in JOBS if job['id'] == job_id), None)
+
+    if not job:
+        return "Job not found", 404
+
+    # Render a template with job details
+    return render_template("jobdetails.html", job=job)
+
+@app.route("/job/<int:job_id>/apply", methods=['POST'])
+def apply_job(job_id):
+    data=request.form
+    job=job_details(job_id)
+    return render_template('application_submitted.html',
+                           application=data,
+                           job=job)'''
 
 @app.route("/api/jobs")
 def list_jobs():
